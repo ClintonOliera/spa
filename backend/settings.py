466ffe417b7956c599030e775ab455b82bc2193f
ClintonOliera <clintonoliera391@gmail.com>
2025-11-14 03:05:19 +0300

    working on database hosts still

--- backend/settings.py
+++ backend/settings.py
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT', '5432'),
+        'NAME': 'lavylotus_db',
+        'USER': 'lavylotus_db_user',
+        'PASSWORD': 'cBcOSCvpkRAEwCuSMZ5QS75OeLFCn0Gu',
+        'HOST': 'dpg-d4b5lse3jp1c73ehdgcg-a.oregon-postgres.render.com',
+        'PORT': '5432',
     }
 }
 
