e0b994394fb0a73bbcf43d7942c5daeffaeca950
ClintonOliera <clintonoliera391@gmail.com>
2025-11-18 22:35:21 +0300

    fixed import issue in urls

--- backend/urls.py
+++ backend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from bookings import views
 from analytics.views import analytics_summary
-from ..bookings.views import CustomTokenView
+from bookings.views import CustomTokenView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
